120de0b0c10cb7c617a73725c2d80f9c5cc5385a
Torben Albert-Lindqvist <torben223@gmail.com>
2020-12-27 18:24:09 +0100

    fix node and yarn conflict

--- server/run.py
+++ server/run.py
@@ -51,4 +51,4 @@ def fetch_resume():
     return send_from_directory('static', 'RESUME.pdf')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
