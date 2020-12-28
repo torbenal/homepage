feda672509806ea0f19abfaebc741aef68785ed5
Torben Albert-Lindqvist <torben223@gmail.com>
2020-12-28 13:16:37 +0100

    fix query endpoint and update favicon

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
       text: queryStr
     }
 
-    axios.post('http://127.0.0.1:5000/query', payload).then(response => {
+    axios.post('/query', payload).then(response => {
       setQueryHistory(prevItems => [...prevItems, response.data.result_text]);
       scrollToBottom()
     }).catch(() => {
